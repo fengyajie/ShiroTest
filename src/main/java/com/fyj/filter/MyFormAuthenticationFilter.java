fe38b34444a1417ce1c3c9cd82ff27f12ec2f40c
Administrator <Administrator@USER-20161117LB>
2018-03-26 21:44:12 +0800

    登陆成功

--- src/main/java/com/fyj/filter/MyFormAuthenticationFilter.java
+++ src/main/java/com/fyj/filter/MyFormAuthenticationFilter.java
@@ -12,19 +12,6 @@ import org.springframework.util.StringUtils;
 
 public class MyFormAuthenticationFilter extends FormAuthenticationFilter{
 
-
-	@Override
-	protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request,
-			ServletResponse response) throws Exception {
-		WebUtils.getAndClearSavedRequest(request);
-		String fallbackUrl = (String)getSubject(request,response).getSession().getAttribute("authc.fallbackUrl");
-	    if(StringUtils.isEmpty(fallbackUrl)){
-	    	fallbackUrl = getSuccessUrl();
-	    }
-	    WebUtils.redirectToSavedRequest(request, response, fallbackUrl);
-		return false;
-	}
-
 	@Override
 	protected void issueSuccessRedirect(ServletRequest request, ServletResponse response) throws Exception {
 		String fallbackUrl = (String)getSubject(request,response).getSession().getAttribute("authc.fallbackUrl");
