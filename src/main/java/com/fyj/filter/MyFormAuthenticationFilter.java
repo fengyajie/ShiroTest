2cfb76e9cb4fc19d72e628dd1b39e0cfd9a0baac
fengyj <fengyj@PC-ITM-016.qiji.local>
2018-03-26 11:39:05 +0800

    认证成功跳转

--- src/main/java/com/fyj/filter/MyFormAuthenticationFilter.java
+++ src/main/java/com/fyj/filter/MyFormAuthenticationFilter.java
@@ -4,12 +4,26 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.apache.shiro.web.util.WebUtils;
 import org.springframework.util.StringUtils;
 
 public class MyFormAuthenticationFilter extends FormAuthenticationFilter{
 
+
+	@Override
+	protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request,
+			ServletResponse response) throws Exception {
+		WebUtils.getAndClearSavedRequest(request);
+		String fallbackUrl = (String)getSubject(request,response).getSession().getAttribute("authc.fallbackUrl");
+	    if(StringUtils.isEmpty(fallbackUrl)){
+	    	fallbackUrl = getSuccessUrl();
+	    }
+	    WebUtils.redirectToSavedRequest(request, response, fallbackUrl);
+		return false;
+	}
 
 	@Override
 	protected void issueSuccessRedirect(ServletRequest request, ServletResponse response) throws Exception {
